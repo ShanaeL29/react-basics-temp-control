38ebb5222530a6b2a4a7074082e3561adea7f3be
Shanae Leslie <sleslie18@gmail.com>
2022-10-26 15:49:59 -0400

    temperature control functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [temp, setTemp] = useState(62);
+  const [tempColor, setTempColor] = useState("cold");
+  let newTemp = temp;
+
+  const colorSet = () => {
+    if (newTemp < 65) {
+      setTempColor("cold");
+    } else if (newTemp >= 65 && newTemp < 80) {
+      setTempColor("warm");
+    } else setTempColor("hot");
+  };
+
+  const increaseTemp = () => {
+    if (temp < 106) {
+      newTemp += 1;
+    }
+    setTemp(newTemp);
+    colorSet(newTemp);
+  };
+
+  const decreaseTemp = () => {
+    if (temp > -3) {
+      newTemp -= 1;
+    }
+    setTemp(newTemp);
+    colorSet(newTemp);
+  };
+
   return (
     <div className="app-container">
       <div className="temperature-display-container">
-        <div className="temperature-display">50°F</div>
+        <div className={`temperature-display ${tempColor}`}>{temp}°F</div>
       </div>
       <div className="button-container">
-        <button>+</button>
-        <button>-</button>
+        <button onClick={increaseTemp}>+</button>
+        <button onClick={decreaseTemp}>-</button>
       </div>
     </div>
   );
